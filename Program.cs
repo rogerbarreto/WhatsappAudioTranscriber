3717a5b3bf2f75b812b20e01372001c4e63181cc
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-04-04 12:25:57 +0100

    Remove unused namespaces

--- Program.cs
+++ Program.cs
@@ -1,13 +1,5 @@
 namespace WhatsappAudioTranscriber;
 
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using Whisper.net;
-using Whisper.net.Ggml;
-using Whisper.net.LibraryLoader;
-using Whisper.net.Logger;
-using static System.Windows.Forms.Design.AxImporter;
-
 internal static class Program
 {
     /// <summary>
